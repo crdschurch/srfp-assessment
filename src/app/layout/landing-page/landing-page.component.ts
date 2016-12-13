e7ba1c19bb10aa7eec30c828c06fa9ec9287732f
zarahzachz <sarah.sachs@gmail.com>
2016-12-13 10:16:11 -0500

    US5903 - DDK Foundation Make landing page reusable for each navigation item.

--- src/app/layout/landing-page/landing-page.component.ts
+++ src/app/layout/landing-page/landing-page.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class LandingPageComponent implements OnInit {
+  landing_page = {
+    'name': 'UI Components',
+    'info': 'Vestibulum id ligula porta felis euismod semper. Donec sed odio dui. Donec sed odio dui. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.'
+  }
 
   constructor() { }
 
