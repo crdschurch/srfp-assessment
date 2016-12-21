27eb44e4aab07a7384c9e0d1b175e7b0d94eb5ef
Taylor C. MacDonald <tcmacdonald@gmail.com>
2016-12-21 15:41:25 -0500

    US5903 - DDK Foundation Implement collapsible mobile nav

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -4,4 +4,6 @@ import { Component } from '@angular/core';
   selector: 'ddk-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public isMenuCollapsed: boolean = true;
+}
