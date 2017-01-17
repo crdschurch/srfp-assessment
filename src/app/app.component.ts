1125593ab983f208581e58fc239831b4618c2f44
Taylor C. MacDonald <tcmacdonald@gmail.com>
2017-01-17 14:25:37 -0500

    US6263 Form Fields Automate example blocks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,31 @@ export class AppComponent implements AfterViewChecked {
   title = 'app works!';
 
   ngAfterViewChecked() {
-    var els = document.getElementsByClassName('language-markup');
-    for(var i = 0; i < els.length; i++) {
-      Prism.highlightElement(els[i]);
+    let examples = document.getElementsByClassName('crds-example');
+    for(var i = 0; i < examples.length; i++) {
+      if(!examples[i].getAttribute('data-processed')) {
+        this.buildExample(examples[i]);
+      }
     }
+  }
+
+  private buildExample(el) {
+    if(el.getAttribute('data-processed')) return;
+    el.setAttribute('data-processed', 'true');
+
+    var html = el.innerHTML.replace(/^\n+|\n+$/g, '');
+    var node = document.createTextNode(html);
+    var pre = document.createElement('pre');
+        pre.classList.add('language-markup');
+        pre.appendChild(node);
+    var figure = document.createElement('figure');
+        figure.classList.add('highlight');
+        figure.appendChild(pre);
+    this.insertAfter(figure, el);
+    Prism.highlightElement(pre);
+  }
+
+  private insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
   }
 }
