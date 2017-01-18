04adfeeef0d0a3396512b549a159427baf6bd338
zarahzachz <sarah.sachs@gmail.com>
2017-01-18 11:28:49 -0500

    US6264 - Buttons Add input dropdown field to vertical flexbox button group.

--- src/app/ui-components/buttons/groups/groups.component.ts
+++ src/app/ui-components/buttons/groups/groups.component.ts
@@ -3,4 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   templateUrl: './groups.component.html'
 })
-export class ButtonGroupsComponent {}
+export class ButtonGroupsComponent {
+  public customInputSelected: boolean = false;
+
+  public selectButton() {
+    this.customInputSelected = false;
+  }
+
+  public selectCustom() {
+    this.customInputSelected = true;
+  }
+}
