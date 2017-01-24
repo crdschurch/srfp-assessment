81b24514d39fe6fd32e6c1904429e8bf59251b52
zarahzachz <sarah.sachs@gmail.com>
2017-01-24 10:46:50 -0500

    DE2693 - Buttons not staying active in browsers Add Angular 2 logic to add .active to active button.

--- src/app/ui-components/buttons/groups/groups.component.ts
+++ src/app/ui-components/buttons/groups/groups.component.ts
@@ -1,11 +1,55 @@
 import { Component } from '@angular/core';
+import { Button } from './button';
+
+const BUTTONS: Button[] = [
+  { label: 'Option 1'},
+  { label: 'Option 2'},
+  { label: 'Option 3'},
+  { label: 'Option 4'}
+]
+
+const BUTTONS2: Button[] = [
+  { label: 'Option 1'},
+  { label: 'Option 2'},
+  { label: 'Option 3'},
+  { label: 'Option 4'}
+]
+
+const BUTTONS3: Button[] = [
+  { label: 'Option 1'},
+  { label: 'Option 2'},
+  { label: 'Option 3'},
+  { label: 'Option 4'}
+]
+
+const BUTTONS4: Button[] = [
+  { label: 'Option 1'},
+  { label: 'Option 2'},
+  { label: 'Option 3'},
+  { label: 'Option 4'}
+]
 
 @Component({
   templateUrl: './groups.component.html'
 })
+
 export class ButtonGroupsComponent {
   public customInputSelected: boolean = false;
-  something: String = 'sssss';
+  buttons = BUTTONS;
+  buttons2 = BUTTONS2;
+  buttons3 = BUTTONS3;
+  buttons4 = BUTTONS4;
+  selectedButton: Button;
+
+  // isActive: boolean = true;
+
+  // toggle() {
+  //   this.isActive = !this.isActive;
+  // }
+
+  onSelect(button: Button): void {
+    this.selectedButton = button;
+  }
 
   public selectButton() {
     this.customInputSelected = false;
