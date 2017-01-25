4518278494186147699d26b16acfbaf3a9f48498
Gabriel Smith <gabriel.smith01@gmail.com>
2017-01-24 20:17:39 -0500

    set ids for buttons

--- src/app/ui-components/buttons/groups/button.ts
+++ src/app/ui-components/buttons/groups/button.ts
@@ -1,1 +1,4 @@
-export class Button { label: string; }
+export class Button {
+  id: string;
+  label: string;
+}
--- src/app/ui-components/buttons/groups/groups.component.ts
+++ src/app/ui-components/buttons/groups/groups.component.ts
@@ -2,31 +2,31 @@ import { Component } from '@angular/core';
 import { Button } from './button';
 
 const BUTTONS: Button[] = [
-  { label: 'Option 1'},
-  { label: 'Option 2'},
-  { label: 'Option 3'},
-  { label: 'Option 4'}
+  { id: 'button1-1', label: 'Option 1'},
+  { id: 'button1-2', label: 'Option 2'},
+  { id: 'button1-3', label: 'Option 3'},
+  { id: 'button1-4', label: 'Option  4'}
 ]
 
 const BUTTONS2: Button[] = [
-  { label: 'Option 1'},
-  { label: 'Option 2'},
-  { label: 'Option 3'},
-  { label: 'Option 4'}
+  { id: 'button2-1', label: 'Option 1'},
+  { id: 'button2-2', label: 'Option 2'},
+  { id: 'button2-3', label: 'Option 3'},
+  { id: 'button2-4', label: 'Option 4'}
 ]
 
 const BUTTONS3: Button[] = [
-  { label: 'Option 1'},
-  { label: 'Option 2'},
-  { label: 'Option 3'},
-  { label: 'Option 4'}
+  { id: 'button3-1', label: 'Option 1'},
+  { id: 'button3-2', label: 'Option 2'},
+  { id: 'button3-3', label: 'Option 3'},
+  { id: 'button3-4', label: 'Option 4'}
 ]
 
 const BUTTONS4: Button[] = [
-  { label: 'Option 1'},
-  { label: 'Option 2'},
-  { label: 'Option 3'},
-  { label: 'Option 4'}
+  { id: 'button4-1', label: 'Option 1'},
+  { id: 'button4-2', label: 'Option 2'},
+  { id: 'button4-3', label: 'Option 3'},
+  { id: 'button4-4', label: 'Option 4'}
 ]
 
 @Component({
@@ -39,7 +39,7 @@ export class ButtonGroupsComponent {
   buttons2 = BUTTONS2;
   buttons3 = BUTTONS3;
   buttons4 = BUTTONS4;
-  selectedButton: Button;
+  selectedButtonId: string;
 
   // isActive: boolean = true;
 
@@ -47,8 +47,14 @@ export class ButtonGroupsComponent {
   //   this.isActive = !this.isActive;
   // }
 
+  isActive(button: Button) {
+    console.log(`Button: ${button.id}`, this.selectedButtonId === button.id);
+    return this.selectedButtonId === button.id;
+  }
+
   onSelect(button: Button): void {
-    this.selectedButton = button;
+    this.selectedButtonId = button.id;
+    console.log('onSelect', this.selectedButtonId);
   }
 
   public selectButton() {
