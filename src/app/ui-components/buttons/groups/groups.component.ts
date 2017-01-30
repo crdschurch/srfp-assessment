dd123079d0fd3baedf9759063050fa163a937cd8
zarahzachz <sarah.sachs@gmail.com>
2017-01-30 16:31:21 -0500

    DE2693 - Buttons not highlighted in browsers Re-implement ability to add .active class to buttons in button groups.

--- src/app/ui-components/buttons/groups/groups.component.ts
+++ src/app/ui-components/buttons/groups/groups.component.ts
@@ -1,17 +1,54 @@
 import { Component } from '@angular/core';
 
+const BUTTONS = [
+  { label: 'Option 1' },
+  { label: 'Option 2' },
+  { label: 'Option 3' },
+  { label: 'Option 4' }
+]
+
+const NO_OUTLINE_BUTTONS = [
+  { label: 'Option 1' },
+  { label: 'Option 2' },
+  { label: 'Option 3' },
+  { label: 'Option 4' }
+]
+
+const FLEX_BUTTONS = [
+  { label: 'Option 1' },
+  { label: 'Option 2' },
+  { label: 'Option 3' },
+  { label: 'Option 4' }
+]
+
+const VERT_BUTTONS = [
+  { label: 'Option 1', amount: '100' },
+  { label: 'Option 2', amount: '200' },
+  { label: 'Option 3', amount: '300' }
+]
+
 @Component({
   templateUrl: './groups.component.html'
 })
 
 export class ButtonGroupsComponent {
   public customInputSelected: boolean = false;
+  buttons = BUTTONS;
+  no_outline_buttons = NO_OUTLINE_BUTTONS;
+  flex_buttons = FLEX_BUTTONS;
+  vert_buttons = VERT_BUTTONS;
+  selectedButton;
+
+  onSelect(button): void {
+    this.selectedButton = button;
+    this.customInputSelected = false;
+  }
 
   public selectButton() {
     this.customInputSelected = false;
   }
 
-  public selectCustom() {
+  public selectedCustom() {
     this.customInputSelected = true;
   }
 }
