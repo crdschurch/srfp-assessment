18182e117afae39e322c8ded7272b25b3a72a26b
Taylor C. MacDonald <tcmacdonald@gmail.com>
2017-01-31 16:53:50 -0500

    Revert "Revert "Feature/us6464 datepicker""
    This reverts commit 22ec17994cb6da1f7ad0898ee6b3b8cf23154a8f.

--- src/app/ui-components/ui-components.module.ts
+++ src/app/ui-components/ui-components.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { TabsModule } from 'ng2-bootstrap';
+import { TabsModule, DatepickerModule } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { UiComponentsComponent } from './ui-components.component';
 import { UiRoutingModule } from './ui-routing.module';
@@ -41,6 +41,7 @@ import { FormsComponent } from './forms/forms.component';
 import { FormsJumbotronComponent } from './forms/jumbotron/jumbotron.component';
 import { FormControlsComponent } from './forms/form-controls/form-controls.component';
 import { FormStatesComponent } from './forms/form-states/form-states.component';
+import { FormDatepickerComponent } from './forms/datepicker/datepicker.component';
 import { FormAlertsComponent } from './forms/alerts/alerts.component';
 import { FormGroupsComponent } from './forms/form-groups/groups.component';
 
@@ -48,7 +49,8 @@ import { FormGroupsComponent } from './forms/form-groups/groups.component';
   imports: [
     CommonModule,
     UiRoutingModule,
-    TabsModule
+    TabsModule,
+    DatepickerModule
   ],
   exports: [
     UiComponentsComponent
@@ -84,6 +86,7 @@ import { FormGroupsComponent } from './forms/form-groups/groups.component';
     FormsJumbotronComponent,
     FormControlsComponent,
     FormStatesComponent,
+    FormDatepickerComponent,
     FormAlertsComponent,
     FormGroupsComponent,
 
