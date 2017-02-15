37b488f2e89a287a40e790562f5d9614740e5b34
Taylor C. MacDonald <tcmacdonald@gmail.com>
2017-02-15 11:43:22 -0500

    DEXXXX Build Problems Another attempt at casting the ENV as a string

--- src/app/shared/content-block/content.service.ts
+++ src/app/shared/content-block/content.service.ts
@@ -18,7 +18,7 @@ export class ContentService {
   }
 
   getContentBlocks (categories: Array<string>) {
-    let cmsEndpoint = <string> '' + process.env.CRDS_CMS_ENDPOINT;
+    let cmsEndpoint = <string> new String(process.env.CRDS_CMS_ENDPOINT);
     let apiUrl = `${cmsEndpoint}api/contentblock`;
 
     if (Array.isArray(categories) && categories.length > 0) {
