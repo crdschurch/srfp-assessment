e14986079aa17b27d892f73c1dd9b45b68d6bfef
Taylor C. MacDonald <tcmacdonald@gmail.com>
2017-02-16 17:06:19 -0500

    US6539 Loading Spinner Remove extraneous spec code.

--- src/app/ui-components/loaders/icons/icons.component.spec.ts
+++ src/app/ui-components/loaders/icons/icons.component.spec.ts
@@ -1,12 +1,5 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async } from '@angular/core/testing';
-import { HttpModule } from '@angular/http';
-import { RouterTestingModule } from '@angular/router/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { LoaderIconsComponent } from './icons.component';
-import { ContentBlockModule } from '../../../shared/content-block/content-block.module';
-
 
 describe('Component: LoaderIconsComponent', () => {
 
@@ -15,14 +8,7 @@ describe('Component: LoaderIconsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        LoaderIconsComponent
-      ],
-      imports: [
-        ContentBlockModule,
-        HttpModule,
-        RouterTestingModule.withRoutes([])
-      ]
+      declarations: [LoaderIconsComponent]
     });
     this.fixture = TestBed.createComponent(LoaderIconsComponent);
     this.component = this.fixture.componentInstance;
