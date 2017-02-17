3a72de4bd0d00dd969904a4c67aba2c13dc3c393
Taylor C. MacDonald <tcmacdonald@gmail.com>
2017-02-17 12:08:08 -0500

    US6466 Typography Settings Add specs for all typography components that do not already have them

---
+++ src/app/ui-components/typography/typesetting/typesetting.component.spec.ts
@@ -0,0 +1,9 @@
+import { TestBed } from '@angular/core/testing';
+import { TypesettingComponent } from './typesetting.component';
+
+describe('Component: FontScaling', () => {
+  it('should create an instance', () => {
+    let component = new TypesettingComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/ui-components/typography/typography-list/typography-list.component.spec.ts
@@ -0,0 +1,9 @@
+import { TestBed } from '@angular/core/testing';
+import { TypographyListComponent } from './typography-list.component';
+
+describe('Component: TypographyList', () => {
+  it('should create an instance', () => {
+    let component = new TypographyListComponent();
+    expect(component).toBeTruthy();
+  });
+});
