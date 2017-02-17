3d3bfa812f0b323bebc1ffd409046933f77182f8
Taylor C. MacDonald <tcmacdonald@gmail.com>
2017-02-17 15:21:26 -0500

    DE2791 Example Links Cleanup specs

--- src/app/ui-components/tables/tables.component.spec.ts
+++ src/app/ui-components/tables/tables.component.spec.ts
@@ -7,32 +7,9 @@ import { TablesJumbotronComponent } from './jumbotron/jumbotron.component';
 import { ContentBlockComponent } from '../../shared/content-block/content-block.component';
 import { ContentService } from '../../shared/content-block/content.service';
 
-describe('Component: Typesetting', () => {
-
-  let component;
-  let fixture;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [TablesComponent, TablesJumbotronComponent, ContentBlockComponent],
-      imports: [],
-      providers: [
-        Renderer,
-        ContentService,
-        MockBackend,
-        BaseRequestOptions,
-        {
-          provide: Http,
-          useFactory: (backend, options) => new Http(backend, options),
-          deps: [MockBackend, BaseRequestOptions]
-        }
-      ]
-    });
-    this.fixture = TestBed.createComponent(TablesComponent);
-    this.component = this.fixture.componentInstance;
-  });
-
+describe('Component: Tables', () => {
   it('should create an instance', () => {
-    expect(this.component).toBeTruthy();
+    let component = new TablesComponent();
+    expect(component).toBeTruthy();
   });
 });
