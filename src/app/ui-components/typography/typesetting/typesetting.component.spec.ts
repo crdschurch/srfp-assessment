470cb818f88865cd624fbefffd8ff171ea59c42e
Taylor C. MacDonald <tcmacdonald@gmail.com>
2017-02-17 14:37:10 -0500

    DE2789 Link Examples Prevent reload of page when clicking link within example blocks

--- src/app/ui-components/typography/typesetting/typesetting.component.spec.ts
+++ src/app/ui-components/typography/typesetting/typesetting.component.spec.ts
@@ -1,9 +1,22 @@
 import { TestBed } from '@angular/core/testing';
+import { ElementRef, Renderer } from '@angular/core';
 import { TypesettingComponent } from './typesetting.component';
 
 describe('Component: Typesetting', () => {
+
+  let component;
+  let fixture;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TypesettingComponent],
+      providers: [Renderer]
+    });
+    this.fixture = TestBed.createComponent(TypesettingComponent);
+    this.component = this.fixture.componentInstance;
+  });
+
   it('should create an instance', () => {
-    let component = new TypesettingComponent();
-    expect(component).toBeTruthy();
+    expect(this.component).toBeTruthy();
   });
 });
--- src/app/ui-components/typography/typesetting/typesetting.component.ts
+++ src/app/ui-components/typography/typesetting/typesetting.component.ts
@@ -1,6 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer } from '@angular/core';
 
 @Component({
   templateUrl: './typesetting.component.html'
 })
-export class TypesettingComponent {}
+export class TypesettingComponent {
+  constructor(elementRef: ElementRef, renderer: Renderer) {
+    renderer.listen(elementRef.nativeElement, 'click', (event) => {
+      if (event.target.closest('.crds-example')) {
+        return false;
+      }
+    });
+  }
+}
