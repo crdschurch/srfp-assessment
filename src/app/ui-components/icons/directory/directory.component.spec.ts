e11321e6ff1e0cb9bc0b78d9e75759732afce0cb
Taylor C. MacDonald <tcmacdonald@gmail.com>
2017-02-23 13:49:15 -0500

    US6973 Add SVGs to DDK Add specs

---
+++ src/app/ui-components/icons/directory/directory.component.spec.ts
@@ -0,0 +1,8 @@
+import { IconDirectoryComponent } from './directory.component';
+
+describe('Component: IconDirectory', () => {
+  it('should create an instance', () => {
+    let component = new IconDirectoryComponent();
+    expect(component).toBeTruthy();
+  });
+});
