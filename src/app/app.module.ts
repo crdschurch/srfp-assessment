fc5c616571b69866af9ded94815d2a8e665c379d
Matt Silbernagel <silbermm@gmail.com>
2017-04-11 11:55:36 -0400

    add rx-auth back and ToastModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,13 +28,14 @@ import { CustomOptions } from './app.toast.options';
     ReactiveFormsModule,
     HttpModule,
     AppRoutingModule,
+    ToastModule.forRoot(),
     ContentBlockModule.forRoot({ endpoint: environment.crdsEndpoint, categories: Array('main', 'common') })
   ],
   providers: [ {
     provide: AuthService,
     useFactory: authServiceFactory,
-    deps: []}
-    , CanActivateViaAuth, 
+    deps: []},
+    CanActivateViaAuth,
     { provide: ToastOptions, useClass: CustomOptions }],
   bootstrap: [AppComponent]
 })
